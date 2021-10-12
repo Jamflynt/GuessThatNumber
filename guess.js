4bfbfc3f3e1606a5ccf8989665e9936ccf43b35c
Jamie Flynt <jamflynt@gmail.com>
2021-10-12 11:12:44 -0400

    Added starting variables to JS file.

--- guess.js
+++ guess.js
@@ -1,1 +1,24 @@
 console.log("Hello World");
+
+// Guess That Number
+// Message to be used throughout the file/project
+const enterNumText = `Please enter a number greater than zero`;
+
+// For restarting the game
+let restartGame = true; 
+
+// For storing the range of the number to be guessed
+let rangeNum; 
+
+// For storing the number to be guessed 
+let randomNum;
+
+// For storing the number of attempts that the user has left 
+let attempt;
+
+// For storing the users guess
+let guess; 
+
+// For storing user's response to play again or not play again
+let playAgain; 
+
