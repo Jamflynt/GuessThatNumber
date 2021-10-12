a32d49fb93276571e6b69324d962075fabd27923
Jamie Flynt <jamflynt@gmail.com>
2021-10-12 12:11:55 -0400

    Range number section complete.

--- guess.js
+++ guess.js
@@ -22,3 +22,17 @@ let guess;
 // For storing user's response to play again or not play again
 let playAgain; 
 
+// Starting alert message 
+alert(`Welcome to "GUESS THAT NUMBER!" Please click "OK" to start the game.`);
+
+// Game restarts as long as restartGame has value of true
+while(restartGame){
+    // Asks user to enter a number to set the upper bound for the random number that will be created (aka Number to be guessed)
+    rangeNum = prompt(`Please enter a maximum number for the range:`);
+
+    // Using parseInt to attempt to convert the user's response into a number value (note: the value returned from a prompt is a string value. Also, if the value cannot be converted then the value returned will be NaN (not a number))
+    rangeNum = parseInt(rangeNum);
+
+    break;
+}
+
